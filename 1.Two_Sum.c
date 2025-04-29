f01078f75b9be3d258b744e548dc77cd585131e5
arthuurca <arthurcontactbr@gmail.com>
2025-04-29 00:21:54 -0300

    Codigo incompleto ainda

--- 1.Two_Sum.c
+++ 1.Two_Sum.c
@@ -20,6 +20,25 @@ HashTable *createTable(int size)
   table->item = (HashItem *)calloc(size, sizeof(HashItem));
 }
 
+int hash(int key, int size)
+{
+  if (key < 0)
+    key = -key;
+  return key % size;
+}
+
+void insert(HashTable *table, int key, int value)
+{
+  int index = hash(key, table->size);
+
+  while (table->item[index].key != 0 || table->item[index].value != 0)
+  {
+    index = (index + 1) % table->size;
+  }
+
+  table->item[index].key = key
+}
+
 int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
 }
